635ec485bcc8c4c4b5ff528709954513867708f3
Aleksandr Rabtsevich <57217014+stavkidisq@users.noreply.github.com>
2022-09-28 00:05:08 +0300

    Update double8.c

--- c-trash/double8.c
+++ c-trash/double8.c
@@ -1,64 +1,151 @@
 #include <iostream>
 
+/*
+    --------------
+   | start_frame  | - Стартовый кадр, который должен быть равен 0x01111110.
+    --------------
+    --------------
+   | first_frame  | - Кадр данных.
+    --------------
+    --------------
+   | second_frame | - Кадр данных.
+    --------------
+    --------------
+   | third_frame  | - Кадр данных.
+    --------------
+    --------------
+   | fourth_frame | - Кадр данных.
+    --------------
+    --------------
+   |  fifth_frame | - Кадр данных.
+    --------------
+    --------------
+   |   xor_frame  | - XOR-кадр, который равен функции XOR от всех кадров данных.
+    --------------
+    --------------
+   |  stop_frame  | - Стоповый кадр, который должен быть равен 0x10000001.
+    --------------
+*/
+
+#define XOR ^
+#define DEVICE_DATA_LENGTH 14
+#define PACKAGE_SIZE 8
+
+// Данные, взятые с 1-го датчика.
 bool frame_from_first_device[] = 
 {
     1, 0, 1, 1, 1, 0, 1, 1 , 0, 0, 1, 0, 0, 0
 };
 
+// Данные, взятые со 2-го датчика.
 bool frame_from_second_device[] =
 {
     1, 1, 0, 1, 1, 1, 1, 1 , 0, 0, 1, 0, 1, 0
 };
 
+// Данные, взятые с 3-го датчика.
 bool frame_from_third_device[] =
 {
     1, 0, 0, 0, 1, 0, 1, 1 , 1, 0, 1, 1, 0, 1
 };
 
-int sender(bool buff[][8], bool frames[14]);
-int receiver(bool package[][8], bool frames[14]);
+// Функция отправителя данных.
+int sender(bool buff_package[][8], bool frame[DEVICE_DATA_LENGTH]);
 
-int encoder(bool arr[], bool buff[][8]);
+// Функция получателя данных.
+int receiver(bool package[][8], bool buff_frame[DEVICE_DATA_LENGTH]);
+
+// Функция кодирования данных.
+int encoder(bool frame[], bool buff_package[][8]);
+
+// Функция декодирования данных.
 int decoder(bool package[][8], bool data_frame[]);
 
+// Функция заполнения кадров с полезной информацией.
 int fill_data_frame(bool arr[], bool buff[], int start, int end);
+
+// Функция вывода полезной информации на консоль.
+int display_useful_data(bool frame[]);
 
 int main()
 {
-    bool first_package[8][8] = {};
-    bool second_package[8][8] = {};
-    bool third_package[8][8] = {};
-
-    bool fourth_package[8][8] = {
-        { 0, 1, 1, 1, 1, 1, 0, 0 },
+    // Здесь происходит инициализация массивов, хранящих пакеты данных.
+    bool first_package[PACKAGE_SIZE][8] = {};
+    bool second_package[PACKAGE_SIZE][8] = {};
+    bool third_package[PACKAGE_SIZE][8] = {};
+    bool fourth_package[PACKAGE_SIZE][8] =
+    {
+        { 0, 1, 1, 1, 1, 1, 1, 0 },
         { 0, 1, 1, 1, 1, 1, 0, 1 },
         { 0, 1, 1, 1, 1, 1, 1, 1 },
         { 0, 1, 1, 1, 1, 1, 1, 1 },
         { 0, 1, 1, 1, 1, 1, 1, 1 },
-        { 0, 1, 1, 1, 1, 1, 1, 1 },
-        { 0, 0, 0, 0, 0, 0, 0, 0 },
-        { 1, 0, 0, 0, 0, 0, 0, 1 }
+        { 0, 1, 1, 1, 1, 1, 1, 1 }
     };
 
+    // Обработка информации отправтелем.
     sender(first_package, frame_from_first_device);
     sender(second_package, frame_from_second_device);
     sender(third_package, frame_from_third_device);
 
-    bool first_frame[14];
-    bool second_frame[14];
-    bool third_frame[14];
-    bool fourth_frame[14];
+    // Инициализация буферов, в которыебудет вкладываться полезная информация.
+    bool first_frame[DEVICE_DATA_LENGTH];
+    bool second_frame[DEVICE_DATA_LENGTH];
+    bool third_frame[DEVICE_DATA_LENGTH];
+    bool fourth_frame[DEVICE_DATA_LENGTH];
 
-    receiver(first_package, first_frame);
-    receiver(second_package, second_frame);
-    receiver(third_package, third_frame);
-    receiver(fourth_package, fourth_frame);
+    // Обработка информации получателем.
+    if (receiver(first_package, first_frame) == 0)
+    {
+        display_useful_data(first_frame);
+    }
+    else
+    {
+        printf("An error occurred while transmitting the package.\n");
+    }
+
+    if (receiver(second_package, second_frame) == 0)
+    {
+        display_useful_data(second_frame);
+    }
+    else
+    {
+        printf("An error occurred while transmitting the package.\n");
+    }
+
+    if (receiver(fourth_package, fourth_frame) == 0)
+    {
+        display_useful_data(fourth_frame);
+    }
+    else
+    {
+        printf("An error occurred while transmitting the package.\n");
+    }
+
+    if (receiver(third_package, third_frame) == 0)
+    {
+        display_useful_data(third_frame);
+    }
+    else
+    {
+        printf("An error occurred while transmitting the package.\n");
+    }
+
+    return 0;
+}
+
+int display_useful_data(bool frame[])
+{
+    for (int i = 0; i < 14; i++)
+        printf("%d", frame[i]);
+    printf("\n");
 
     return 0;
 }
 
 int decoder(bool package[][8], bool data_frame[])
 {
+    // Перемещение по 3 бита полезной информации в data_frame (c 1-го по 4 кадр включительно).
     int k = 0;
     for (int i = 1; i < 5; i++)
     {
@@ -69,6 +156,7 @@ int decoder(bool package[][8], bool data_frame[])
         }
     }
 
+    // Перемещение 2 оставшихся бит полезной информации в data_frame (5 кадра)
     for (int i = 0; i < 2; i++)
     {
         data_frame[k] = package[5][i];
@@ -78,86 +166,96 @@ int decoder(bool package[][8], bool data_frame[])
     return 0;
 }
 
-int receiver(bool package[][8], bool frames[14])
+int receiver(bool package[][8], bool buff_frame[DEVICE_DATA_LENGTH])
 {
     bool start_frame[8] = { 0, 1, 1, 1, 1, 1, 1, 0 };
     bool stop_frame[8] = { 1, 0, 0, 0, 0, 0, 0, 1 };
     bool xor_frame[8];
 
     for (int i = 0; i < 8; i++)
-        xor_frame[i] = package[1][i] ^ package[2][i] ^ package[3][i] ^ package[4][i] ^ package[5][i];
+        xor_frame[i] = package[1][i] XOR package[2][i] XOR package[3][i] XOR package[4][i] XOR package[5][i];
 
+    // Проверка правильности сартового кадра.
     if (memcmp(package[0], start_frame, 8) < 0)
     {
-        perror("Start frame don't match!");
+        perror("The start frame is incorrect.");
         return -1;
     }
 
+    // Проверка правильности XOR-кадра.
     if (memcmp(package[6], xor_frame, 8) < 0)
     {
-        perror("Xor don't match!");
+        perror("The checksum of data packages is incorrect.");
         return -1;
     }
 
+    // Проверка правильности стопового кадра.
     if (memcmp(package[7], stop_frame, 8) < 0)
     {
-        perror("Stop frame don't match!");
+        perror("The stop frame is incorrect.");
         return -1;
     }
 
-    decoder(package, frames);
+    decoder(package, buff_frame);
+
     return 0;
 }
 
-int sender(bool buff[][8], bool frames[14])
+int sender(bool buff_package[][8], bool frame[DEVICE_DATA_LENGTH])
 {
     bool arr[8][8] = {};
-    int result = encoder(frames, arr);
+    encoder(frame, arr);
 
+    // Перемещение данных в буффер.
     for (int i = 0; i < 8; i++)
     {
         for (int j = 0; j < 8; j++)
         {
-            buff[i][j] = arr[i][j];
+            buff_package[i][j] = arr[i][j];
         }
     }
 
     return 0;
 }
 
-int encoder(bool arr[], bool buff[][8])
+int encoder(bool frame[], bool buff_package[][8])
 {
+    // Задаются стартовый и стоповый кадры.
     bool start_frame[8] = { 0, 1, 1, 1, 1, 1, 1, 0 };
     bool stop_frame[8] = { 1, 0, 0, 0, 0, 0, 0, 1 };
     
+    // Заполнение стартового кадра.
     for (int i = 0; i < 8; i++)
-        buff[0][i] = start_frame[i];
+        buff_package[0][i] = start_frame[i];
 
-    fill_data_frame(arr, buff[1], 0, 3);
-    fill_data_frame(arr, buff[2], 3, 6);
-    fill_data_frame(arr, buff[3], 6, 9);
-    fill_data_frame(arr, buff[4], 9, 12);
-    fill_data_frame(arr, buff[5], 12, 14);
+    // Заполнение каждого кадра полезной информацией.
+    fill_data_frame(frame, buff_package[1], 0, 3);
+    fill_data_frame(frame, buff_package[2], 3, 6);
+    fill_data_frame(frame, buff_package[3], 6, 9);
+    fill_data_frame(frame, buff_package[4], 9, 12);
+    fill_data_frame(frame, buff_package[5], 12, 14);
 
+    // Заполнение XOR-кадра.
     for (int i = 0; i < 8; i++)
-        buff[6][i] = buff[1][i] ^ buff[2][i] ^ buff[3][i] ^ buff[4][i] ^ buff[5][i];
+        buff_package[6][i] = buff_package[1][i] XOR buff_package[2][i] XOR buff_package[3][i] XOR buff_package[4][i] XOR buff_package[5][i];
 
+    // Заполнение стопового кадра.
     for (int i = 0; i < 8; i++)
-        buff[7][i] = stop_frame[i];
+        buff_package[7][i] = stop_frame[i];
 
     return 0;
 }
 
 int fill_data_frame(bool arr[], bool buff[], int start, int end)
 {
+    // Заполнение кадра мусорными битами.
     for (int i = 0; i < 8; i++)
         buff[i] = i % 2;
 
+    // Заполнение кадра полезными битами.
     int j = 0;
     for (int i = start; i < end; i++, j++)
-    {
         buff[j] = arr[i];
-    }
 
     return 0;
 }
